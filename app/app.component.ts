55a4bf2d666271727b9650088d675f55dd81b1d8
jyana <jordan.yana@gmail.com>
2015-12-19 22:02:10 -0500

    Sample Angular2 Framework

---
+++ app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component, View} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
+import {TemplateComp} from './test-comp';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: "app/views/home/home.html",
+  directives: [HeroDetailComponent, TemplateComp]
+})
+export class AppComponent {
+	public title = 'Tour of Heroes';
+  public heroes = HEROES;
+  public selectedHero: Hero;
+
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
---
+++ app/test-comp.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'test-this',
+  template: '<h1>hello</h1>'
+})
+export class TemplateComp {
+}
